938fcb6d87076d1263da3cdeb1cab9141e19d098
jin <rhos0129@gmail.com>
2022-03-19 20:59:33 +0900

    RecipeFinder : 선택메뉴의 레시피 보기 데이터를 localStorage를 이용하여 전달

--- Vanilla/RecipeFinder/main.js
+++ Vanilla/RecipeFinder/main.js
@@ -5,6 +5,19 @@ const results = document.querySelector('.result-container');
 const meal = document.querySelector('.meal');
 const searchBtn = document.querySelector('.search-btn');
 
+// popup창 : localStorage에 데이터가 있다면 해당id의 레시피 읽기
+if(localStorage.getItem('id')){ 
+    var id = localStorage.getItem('id') ;
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then(res => res.json())
+    .then(data => {
+        var mealData=data.meals[0];
+        document.querySelector('.popup-header h3').innerText = mealData.strMeal;
+        document.querySelector('.popup-img img').setAttribute('src', mealData.strMealThumb);
+        document.querySelector('.popup-content p').innerText = mealData.strInstructions;
+    })
+}
+
 
 // functions
 function search(keyword){
@@ -14,13 +27,13 @@ function search(keyword){
         var i=0
         while(i<data.meals.length){
             var mealData=data.meals[i];
-            addResult(mealData.strMeal, mealData.strMealThumb);
+            addResult(mealData.strMeal, mealData.strMealThumb, mealData.idMeal)
             i=i+1;
         }
     }) // 서버에서 주는 json 데이터 읽기 
 }
 
-function addResult(name, imgUrl){
+function addResult(name, imgUrl, id){
     var meal=document.createElement("div");
     meal.classList.add("meal");
     
@@ -40,7 +53,8 @@ function addResult(name, imgUrl){
     mealPopupBtn.classList.add("meal-popup-btn");
     mealPopupBtn.innerText="자세히 보기";
     mealPopupBtn.onclick = function(){
-        window.open('./popup.html', 
+        localStorage.setItem('id', id); // localStorage에 데이터 저장
+        window.open('./popup.html',
             'recipe', 
             'left=130, bottom=100, location=no, resizable=no');
     }
