edb48c4476980c99fc6fff5a051aaea3a988118b
jin <rhos0129@gmail.com>
2022-03-19 18:02:35 +0900

    Typing : 단어 일치 확인

---
+++ Vanilla/Typing/main.js
@@ -0,0 +1,15 @@
+// variables
+let score = 0;
+
+const wordInput = document.querySelector('.word-input');
+const wordDisplay = document.querySelector('.word-display');
+const scoreDisplay=document.querySelector('.score');
+
+// events
+wordInput.addEventListener('input', () => {
+    if(wordInput.value.toLowerCase() === wordDisplay.innerText.toLowerCase()){
+        score++;
+        scoreDisplay.innerText=score;
+        wordInput.value='';
+    }
+})
