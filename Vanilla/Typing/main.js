1fc12c6cecb62879b15f9b63ce1d3ea01642240c
jin <rhos0129@gmail.com>
2022-03-19 18:03:50 +0900

    Typing : 타이머

--- Vanilla/Typing/main.js
+++ Vanilla/Typing/main.js
@@ -1,9 +1,12 @@
 // variables
 let score = 0;
+let time = 5;
+let isPlaying=false;
 
 const wordInput = document.querySelector('.word-input');
 const wordDisplay = document.querySelector('.word-display');
 const scoreDisplay=document.querySelector('.score');
+const timeDisplay=document.querySelector('.time');
 
 // events
 wordInput.addEventListener('input', () => {
@@ -13,3 +16,11 @@ wordInput.addEventListener('input', () => {
         wordInput.value='';
     }
 })
+
+setInterval(countDown, 1000);
+
+// functions
+function countDown(){
+    time > 0 ? time-- : isPlaying = false;
+    timeDisplay.innerText=time;
+}
