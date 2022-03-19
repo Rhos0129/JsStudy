1fd992085eeb14f17c22b72dd56b87268cb68dfb
jin <rhos0129@gmail.com>
2022-03-19 20:48:42 +0900

    RecipeFinder : 메뉴 검색 fetch()를 이용하여 MealApi에서 키워드로 검색 Dom을 조작하여 검색결과 보여주기

---
+++ Vanilla/RecipeFinder/main.js
@@ -0,0 +1,55 @@
+// variables
+var keyword = document.querySelector('.search');
+
+const results = document.querySelector('.result-container');
+const meal = document.querySelector('.meal');
+const searchBtn = document.querySelector('.search-btn');
+
+
+// functions
+function search(keyword){
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`) // api 요청
+    .then(res => res.json()) // propmise 객체로 반환
+    .then(data => {
+        var i=0
+        while(i<data.meals.length){
+            var mealData=data.meals[i];
+            addResult(mealData.strMeal, mealData.strMealThumb);
+            i=i+1;
+        }
+    }) // 서버에서 주는 json 데이터 읽기 
+}
+
+function addResult(name, imgUrl){
+    var meal=document.createElement("div");
+    meal.classList.add("meal");
+    
+    var mealName=document.createElement("div");
+    mealName.classList.add("meal-name");
+    var mealNameText=document.createElement("h4");
+    mealNameText.innerText= name;
+    mealName.appendChild(mealNameText);
+
+    var mealImg=document.createElement("div");
+    mealImg.classList.add("meal-img");
+    var mealImgUrl=document.createElement("img");
+    mealImgUrl.setAttribute('src', imgUrl);
+    mealImg.appendChild(mealImgUrl);
+
+    meal.appendChild(mealName);
+    meal.appendChild(mealImg);
+    results.appendChild(meal);   
+}
+
+function resultsClear(){
+    while(results.hasChildNodes()){
+        results.removeChild(results.firstChild);
+    }
+}
+
+// events
+searchBtn.addEventListener('click', e => {
+    resultsClear();
+    search(keyword.value);
+})
+
