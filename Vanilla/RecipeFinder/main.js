8b857e899964a940325ff37414c96fc2639986b2
jin <rhos0129@gmail.com>
2022-03-19 22:16:11 +0900

    RecipeFinder : 리팩토링(유튜브 참고) while문 대신 forEach() result추가시 innerHTML 활용 검색결과가 없는 경우 처리

--- Vanilla/RecipeFinder/main.js
+++ Vanilla/RecipeFinder/main.js
@@ -2,19 +2,7 @@
 var keyword = document.querySelector('.keyword');
 const resultContainer = document.querySelector('.result-container');
 const searchBtn = document.querySelector('.search-btn');
-
-// popup창 : localStorage에 데이터가 있다면 해당id의 레시피 읽기
-if(localStorage.getItem('id')){ 
-    var id = localStorage.getItem('id');
-    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-    .then(res => res.json())
-    .then(data => {
-        var mealData=data.meals[0];
-        document.querySelector('.popup-header h3').innerText = mealData.strMeal;
-        document.querySelector('.popup-img img').setAttribute('src', mealData.strMealThumb);
-        document.querySelector('.popup-content p').innerText = mealData.strInstructions;
-    })
-}
+const resultText = document.querySelector('.result-text');
 
 
 // functions
@@ -22,46 +10,52 @@ function search(keyword){
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`) // api 요청
     .then(res => res.json()) // propmise 객체로 반환
     .then(data => {
-        var i=0
-        while(i<data.meals.length){
-            var mealData=data.meals[i];
-            addResult(mealData.strMeal, mealData.strMealThumb, mealData.idMeal);
-            i=i+1;
+        var mealData=data.meals;
+        if(mealData){
+            resultText.innerHTML='';
+            mealData.forEach(meal => addResult(meal.strMeal, meal.strMealThumb, meal.idMeal))
+        } else{
+            resultText.innerHTML="<h3> 검색결과가 없습니다. </h3>";
         }
-    }) // 서버에서 주는 json 데이터 읽기 
+    }
+    ) // 서버에서 주는 json 데이터 읽기 
+}
+
+function openPopup(id){
+    localStorage.setItem('id', id);
+    window.open('./popup.html', 
+    'recipe', 
+    'left=130, bottom=100, location=no, resizable=no');
 }
 
 function addResult(name, imgUrl, id){
-    var result=document.createElement("div")
-    result.classList.add("result")
-    
-    var mealName=document.createElement("div");
-    mealName.classList.add("meal-name");
-    var mealNameText=document.createElement("h4");
-    mealNameText.innerText= name;
-    mealName.appendChild(mealNameText);
-
-    var mealImg=document.createElement("div");
-    mealImg.classList.add("meal-img");
-    var mealImgUrl=document.createElement("img");
-    mealImgUrl.setAttribute('src', imgUrl);
-    mealImg.appendChild(mealImgUrl);
-
-    var mealPopupBtn=document.createElement("button");
-    mealPopupBtn.classList.add("meal-popup-btn");
-    mealPopupBtn.innerText="자세히 보기";
-    mealPopupBtn.onclick = function(){
-        localStorage.setItem('id', id); // localStorage에 데이터 저장
-        window.open('./popup.html',
-            'recipe', 
-            'left=130, bottom=100, location=no, resizable=no');
-    }
-
-    result.appendChild(mealName);
-    result.appendChild(mealImg);
-    result.appendChild(mealPopupBtn);
-    resultContainer.appendChild(result);   
+    html=`
+    <div class="result">
+        <div class="meal-name">
+            <h4>${name}</h4>
+        </div>
+        <div class="meal-img">
+            <img src="${imgUrl}" alt="">
+        </div>
+        <button class="meal-popup-btn" onclick="openPopup(${id})";>자세히 보기</button>
+    </div>`;
+    resultContainer.innerHTML+=html
 }
+
+// popup창 : localStorage에 데이터가 있다면 해당id의 레시피 읽기
+(function(){
+    if(localStorage.getItem('id')){ 
+        var id = localStorage.getItem('id');
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then(res => res.json())
+        .then(data => {
+            var mealData=data.meals[0];        
+            document.querySelector('.popup-header h3').innerText = mealData.strMeal;
+            document.querySelector('.popup-img img').setAttribute('src', mealData.strMealThumb);
+            document.querySelector('.popup-content p').innerText = mealData.strInstructions;
+        })
+    }
+})();
 
 function resultsClear(){
     while(resultContainer.hasChildNodes()){
