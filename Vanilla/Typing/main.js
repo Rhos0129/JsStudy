0a4157bbf4c0918b465adaee571edc4434f1ce51
jin <rhos0129@gmail.com>
2022-03-19 18:08:59 +0900

    Typing : 버튼이벤트로 타이머 설정

--- Vanilla/Typing/main.js
+++ Vanilla/Typing/main.js
@@ -1,12 +1,16 @@
 // variables
+const GAME_TIME=5;
+
 let score = 0;
-let time = 5;
+let time = GAME_TIME;
 let isPlaying=false;
+let timeInterval;
 
 const wordInput = document.querySelector('.word-input');
 const wordDisplay = document.querySelector('.word-display');
 const scoreDisplay=document.querySelector('.score');
 const timeDisplay=document.querySelector('.time');
+const button=document.querySelector('.button');
 
 // events
 wordInput.addEventListener('input', () => {
@@ -17,10 +21,24 @@ wordInput.addEventListener('input', () => {
     }
 })
 
-setInterval(countDown, 1000);
+buttonChange('게임시작')
 
 // functions
 function countDown(){
-    time > 0 ? time-- : isPlaying = false;
     timeDisplay.innerText=time;
+    if(!isPlaying){
+        clearInterval(timeInterval);
+    }
+    time > 0 ? time-- : isPlaying = false;
+}
+
+function buttonChange(text){
+    button.innerText=text;
+    text==='게임시작' ? button.classList.remove('loading'): button.classList.add('loading');
+}
+
+function run(){ 
+    isPlaying=true;
+    time = GAME_TIME;
+    timeInterval = setInterval(countDown, 1000);
 }
