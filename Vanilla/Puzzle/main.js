83ba2b3cfb628eb36e03d016a5f9f67acc2849fd
jin <rhos0129@gmail.com>
2022-03-19 19:11:40 +0900

    Puzzle : Drag&Drop

--- Vanilla/Puzzle/main.js
+++ Vanilla/Puzzle/main.js
@@ -7,6 +7,12 @@ const playTime = document.querySelector(".play-time");
 const tileCount = 16;
 let tiles = [];
 
+const dragged = {
+    el: null,
+    class: null, // 타일 생성시 부여한 className
+    index: null // 섞인 후 타일의 위치
+}
+
 setGame();
 
 
@@ -14,6 +20,7 @@ setGame();
 
 // 타일 섞기
 function setGame(){
+    container.innerHTML = "";
     tiles = createImageTiles();
     tiles.forEach(tile => container.appendChild(tile));
     setTimeout(() => {
@@ -27,7 +34,8 @@ function createImageTiles(){
     const tempArray=[]; 
     Array(tileCount).fill().forEach((_, i) => {
         const li = document.createElement("li");
-        li.setAttribute('date-index', i);
+        li.setAttribute('data-index', i);
+        li.setAttribute('draggable', 'true');
         li.classList.add(`list${i}`); // Template String >> 백틱` + ${변수명}
         tempArray.push(li);
     })
@@ -44,3 +52,38 @@ function shuffle(array){
     }
     return array;
 }
+
+
+// events
+
+// Drag & Drops : li에서 drag설정을 해야 깔끔하게 잘 된다.
+container.addEventListener('dragstart', e => { // 택한 타일 설정
+    const obj=e.target // 옮기려고 택한 타일
+    dragged.el = obj;
+    dragged.class = obj.className;
+    dragged.index = [ ...obj.parentNode.children].indexOf(obj); // indexOf()는 대상이 배열이여야 한다. 원래는 배열형태의 Object이므로 es6문법으로 강제로 배열로 변경
+})
+container.addEventListener('dragover', e => {
+    e.preventDefault() // drop이벤트가 발생할 수 있도록 기본동작 막기
+})
+container.addEventListener('drop', e => { 
+    const obj=e.target; // 옮길 위치의 타일
+    
+    if(obj.className !== dragged.class){ // 타일을 옮기면 내부코드 실행
+        let originPlace;
+        let isLast = false;
+
+        console.log({dragged})
+    
+        if(dragged.el.nextSibling){
+            originPlace = dragged.el.nextSibling; // 타일을 변경하면 원래위치의 타일은 뒤로 밀리므로!
+        } else {
+            originPlace = dragged.el.previousSibling; // 마지막 타일을 택하면 다음타일이 없으므로.. 앞타일을 저장
+            isLast = true;
+        }
+
+        const droppedIndex = [ ...obj.parentNode.children].indexOf(obj);
+        dragged.index > droppedIndex ? obj.before(dragged.el) : obj.after(dragged.el); // Drag한 타일을 Drop한 위치 앞, 뒤에 넣기 -> 타일이 뒤로 한칸이 밀린다.
+        isLast ? originPlace.after(obj) : originPlace.before(obj); // Drag한 타일과 Drop할 위치의 타일의 위치만 바꾼다.
+    }
+})
